c493450d35e42c8269739aaefa9f7f6bd3c1cc06
tphan106 <tphan7591@gmail.com>
2020-08-20 10:55:24 -0700

    Update firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgAXDkyWeUJNqm2g2TSzdx5YDwSuUaJg8",
+    authDomain: "chat-app-b73a9.firebaseapp.com",
+    databaseURL: "https://chat-app-b73a9.firebaseio.com",
+    projectId: "chat-app-b73a9",
+    storageBucket: "chat-app-b73a9.appspot.com",
+    messagingSenderId: "879714292694",
+    appId: "1:879714292694:web:4e6fd0bde296a60b4c4a77",
+    measurementId: "G-X0DLBLD9RM"
+  };

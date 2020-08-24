12d2dd31da6f51dbd0b00511f92cd79277084a25
tphan106 <tphan7591@gmail.com>
2020-08-23 17:42:50 -0700

    Update icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./Sidebar.css";
+import {Avatar} from "@material-ui/core"
+import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Side bar</h1>
+            <div className="sidebar_header">
+                <Avatar />
+                <div className="sidebear_headerRight">
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>
+            </div>
+            <div className="sidebar_search">
+
+            </div>
+            <div className="sidebar_chats">
+
+            </div>
+
         </div>
     )
 }

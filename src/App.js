7d16307ba035fef050b090bd6736e2440c2cfe22
tphan106 <tphan7591@gmail.com>
2020-08-28 16:00:12 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat"
+import { Router } from '@material-ui/icons';
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
         {/*Side bar*/}
+       <Router>
         <Sidebar />
         <Chat />
+        </Router>
       </div>
     </div>
   );

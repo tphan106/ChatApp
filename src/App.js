f487085ea4ef406566ab2404578b22ba9b6d0541
tphan106 <tphan7591@gmail.com>
2020-08-29 18:25:56 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from 'react';
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat"
-import { Router } from '@material-ui/icons';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
       <div className="app_body">
         {/*Side bar*/}
-       <Router>
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Switch>
+            <Sidebar />
+
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
         </Router>
       </div>
     </div>
--- src/Chat.js
+++ src/Chat.js
@@ -6,10 +6,12 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import MicIcon from '@material-ui/icons/Mic'
+import { useParams } from 'react-router-dom';
 
 function Chat() {
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState('');
+    const {roomId} = useParams();
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
